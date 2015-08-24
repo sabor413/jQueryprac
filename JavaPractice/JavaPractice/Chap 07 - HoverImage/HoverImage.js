077eef8176c27c13ea6f5ea070048175b8b0f036
sabor413 <phiota413@gmail.com>
2015-08-24 07:05:42 -0400

    Completed Chapter 7 jQuery hover image exercise

---
+++ JavaPractice/JavaPractice/Chap 07 - HoverImage/HoverImage.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $("#image_rollovers img").each(function () {
+        var oldURL = $(this).attr("src");   // gets the src attribute
+        var newURL = $(this).attr("id");    // gets the id attribute
+
+        //preload images
+        var rolloverImage = new Image();
+        rolloverImage.src = newURL;
+
+        // set up event handlers
+        $(this).hover(
+            function () {
+                $(this).attr("src", newURL);    // sets the src sttribute
+            },
+            function() {
+                $(this).attr("src", oldURL);    // sets the src sttribute
+            }
+        ); // end hover
+    }); // end each
+}); // end ready

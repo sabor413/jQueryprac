a368442c533af2f93a635ff1a41402c21356d816
sabor413 <phiota413@gmail.com>
2015-08-29 16:29:15 -0400

    Added exercise from Chapter 8 I worked on today

---
+++ JavaPractice/JavaPractice/Chap 08 - jQSlideShow/jQSlideShow.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    //create an array of the slide images
+    var image, imageCache = [];
+    $("#slides img").each(function() {
+        image = new Image();
+        image.src= $(this).attr("src");
+        image.title = $(this).attr("alt");
+        imageCache.push(image);
+    }); // end slide image load
+    
+    var imageCounter = 0;
+    var nextImage;
+    var timer = setInterval(
+            function () {
+                $("#caption").fadeOut(1000);
+                $("#slide").fadeOut(1000,
+                    function () {
+                        imageCounter = (imageCounter + 1) % imageCache.length;
+                        nextImage = imageCache[imageCounter];
+                        $("#slide").attr("src", nextImage.src).fadeIn(1000);
+                        $("#caption").text(nextImage.title).fadeIn(1000);
+                    }); // end slide functionality
+            }, 3000); // end setInterval function
+}); // end ready

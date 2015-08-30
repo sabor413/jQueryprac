94d3a66399b247cfb994e8e7da78761d2feaaaee
sabor413 <phiota413@gmail.com>
2015-08-30 09:47:29 -0400

    Adding second javascript for Chap 08 Slide Show exercise

---
+++ JavaPractice/JavaPractice/Chap 08 - jQSlideShow/jQSlideShow2.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    var nextSlide = $("#slides img:first-child");
+    var nextCaption
+    var nextSlideSource;
+    // start slide show
+    setInterval(
+            function() {
+                if (nextSlide.next().length == 0) {
+                    nextSlide = $("#slides img:first-child");
+                }
+                else {
+                    nextSlide = nextSlide.attr("src");
+                }
+                nextSlideSource = nextSlide.attr("src");
+                nextCaption = nextSlide.attr("alt");
+                $("#slide").attr("src", nextSlideSource).fadeIn(1000);
+                $("#caption").text(nextCaption).fadeIn(1000);
+            }, 3000)// end setInterval
+});// end ready function

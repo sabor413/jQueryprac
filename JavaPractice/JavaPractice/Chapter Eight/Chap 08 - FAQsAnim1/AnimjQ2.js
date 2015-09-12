bc4405783779070f2ec9f32d0d70037380a034ca
sabor413 <phiota413@gmail.com>
2015-09-12 15:13:41 -0400

    Completed first animation example for Chapter 08

---
+++ JavaPractice/JavaPractice/Chapter Eight/Chap 08 - FAQsAnim1/AnimjQ2.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $("#faqs h2").click(
+        function () {
+            $(this).toggleClass("minus");
+            $(this).next().slideToggle(1000);
+        }
+    ); // end toggle
+
+    $("#faqs h1").animate(
+            { fontSize: "275%", opacity: 1, left: "+=175" }, 2000
+            ,function () {
+                $("#faqs h2").next().fadeIn(1000).fadeOut(1000);
+            }
+        ); // end animate
+}); // end ready

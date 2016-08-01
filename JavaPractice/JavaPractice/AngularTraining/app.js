655e3cb30faa37f91c21c0ff6692ec2c1ca16c3a
sabor413 <phiota413@gmail.com>
2016-08-01 18:37:54 -0400

    Upadting with latest Angular training

--- JavaPractice/JavaPractice/AngularTraining/app.js
+++ JavaPractice/JavaPractice/AngularTraining/app.js
@@ -3,10 +3,26 @@
 
     app.controller('StoreController', function () {
         this.products = gem;
-        this.tab = 1;
+    });
 
-        this.selectTab = function (setTab) {
+    app.controller("PanelController", function () {
+        this.tab = 1;
+        
+        this.selectTab = function(setTab) {
             this.tab = setTab;
+        };
+
+        this.isSelected = function (checkTab) {
+            return this.tab === checkTab;
+        };
+    });
+
+    app.controller("ReviewController", function () {
+        this.review = {};
+
+        this.addReview = function (product) {
+            product.reviews.push(this.review);
+            this.review = {};
         };
     });
 
@@ -16,14 +32,33 @@
             price: 2.95,
             description: 'Some gems have hidden qualities beyond\n their luster, beyond their shine... Dodeca is\none of those gems.',
             canPurchase: true,
-            soldOut: false
+            soldOut: false,
+            reviews: [
+                {
+                    stars: 5,
+                    body: "I love this product!",
+                    author: "joe@thomas.com"
+                },
+                {
+                    stars: 1,
+                    body: "This product sucks",
+                    author: "tim@hater.com"
+                }
+            ]
         },
         {
             name: 'Pentagonal Gem',
             price: 5.95,
             description: 'Pentagonal gems have five sides',
             canPurchase: true,
-            soldOut: false
+            soldOut: false,
+            reviews: [
+                {
+                    stars: 2,
+                    body: "This product is aww-right!",
+                    author: "carlos@theman.com"
+                }
+            ]
         }];
 })();
 

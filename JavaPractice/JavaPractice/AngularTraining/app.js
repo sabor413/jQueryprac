636e7240ae3827a5848e37033c4696585d7491bc
sabor413 <phiota413@gmail.com>
2016-08-02 11:02:20 -0400

    Continued Starting with Angular course

--- JavaPractice/JavaPractice/AngularTraining/app.js
+++ JavaPractice/JavaPractice/AngularTraining/app.js
@@ -17,12 +17,26 @@
         };
     });
 
-    app.controller("ReviewController", function () {
-        this.review = {};
+    app.directive('reviewForm', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'review-form.html',
+            controller: function() {
+                this.review = {};
 
-        this.addReview = function (product) {
-            product.reviews.push(this.review);
-            this.review = {};
+                this.addReview = function (product) {
+                    product.reviews.push(this.review);
+                    this.review = {};
+                };
+            },
+            controllerAs: 'reviewCtrl'
+        };
+    });
+
+    app.directive('productTitle', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-title.html'
         };
     });
 
